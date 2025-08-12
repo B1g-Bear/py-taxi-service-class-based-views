32197d54d5d02df6a11033509c8d52463ef9d6f9
Andrii Novikov <forytube2023@gmail.com>
2025-08-12 14:08:58 +0300

    Update

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.db.models import Prefetch
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +15,35 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    paginate_by = 5
+    queryset = Car.objects.select_related("manufacturer").all().order_by("id")
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+    queryset = Driver.objects.all().order_by("username")
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related(
+        Prefetch(
+            "cars",
+            queryset=Car.objects.select_related("manufacturer")
+        )
+    )
